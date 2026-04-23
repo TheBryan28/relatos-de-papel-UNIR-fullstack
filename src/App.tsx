c21bb85fe43d8829226e13abfec485cb23c64742
Bryan Cruz <bryan_sca386@outlook.com>
2026-04-22 20:33:30 -0500

    commimt de prueba

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,7 @@ function App() {
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
         </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+      
       </section>
 
       <div className="ticks"></div>
